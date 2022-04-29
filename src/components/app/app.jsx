134e0b1ef306349fe346ede443f592ceb3af3b13
je-joue <maria.v.bushkova@yandex.ru>
2022-04-30 02:49:35 +0300

    header done

---
+++ src/components/app/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './app.module.css';
+import AppHeader from '../app-header/app-header'
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader />
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/app/app';
 import reportWebVitals from './reportWebVitals';
 import {Button} from '@ya.praktikum/react-developer-burger-ui-components';
+
 
 ReactDOM.render(
   <React.StrictMode>
