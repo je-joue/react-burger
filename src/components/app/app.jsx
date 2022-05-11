717a8ebbadc4528c953b33c66aab8c80c9d38bf8
je-joue <maria.v.bushkova@yandex.ru>
2022-05-11 21:30:10 +0300

    API

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './app.module.css';
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
-
-import { ingredients } from '../../utils/data';
+import {apiConfig} from '../../constants/api-config';
 
 function App() {
+  // states
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    getIngredients();
+  }, []);
+
+  const checkResponse = (res) => {
+    if (res.ok) {
+      return res.json()
+    }
+    return Promise.reject(`Ошибка: ${res.status}`)
+  }
+
+  // getCards() {
+  //   return fetch(`${this.baseURL}/cards`, {
+  //     headers: this.headers
+  //   })
+  //   .then(res => this._checkResponse(res));
+  // }
+
+  // Promise.all([api.getUserData(), api.getCards()])
+  // .then(([userData, cards]) => {
+  //   userId = userData._id;
+  //   userInfo.setUserInfo(userData);
+  //   userInfo.setAvatar(userData);
+  //   cardsList.renderItems(cards);
+  // })
+  // .catch(err => console.log(err))
+
+  const getIngredients = () => {
+    fetch(`${apiConfig.baseURL}ingredients`, {
+      headers: apiConfig.headers
+    })
+    .then(res => checkResponse(res))
+    .then(res => setIngredients(res.data))
+    .catch(err => console.log(err));
+  }
+
   return (
     <div className={styles.app}>
       <AppHeader />
---
+++ src/constants/api-config.js
@@ -0,0 +1,6 @@
+export const apiConfig = {
+  baseURL: 'https://norma.nomoreparties.space/api/',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
